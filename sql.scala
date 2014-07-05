048007ac3f41ec899b538f3dd584a14a44d3683d
Jared Roesch <roeschinc@gmail.com>
2014-07-05 03:08:18 -0700

    Add more experiments

--- sql.scala
+++ sql.scala
@@ -1,12 +1,34 @@
 import shapeless._
+import record._
+import shapeless.ops.hlist._
+import ops.record._
+import syntax.typeable._
 import scala.reflect.runtime.universe._
 
-trait SQLBacked[T]
+trait SQLBacked[T] {
+  type Repr <: HList
+  type Keys <: HList
+  
+  val className: String
+  val tableName: String
+  val keys: Keys
+}
 
 object SQLBacked {
-  def apply[T](implicit sql: SQLBacked[T]): SQLBacked[T] = sql
+  def apply[T](implicit sql: SQLBacked[T]): Aux[T, sql.Repr, sql.Keys] = sql
+
+  type Aux[T, R <: HList, K <: HList] = SQLBacked[T] { type Repr = R; type Keys = K }
   
-  implicit def sqlbacked[T](implicit labelgen: LabelledGeneric[T]): SQLBacked[T] = new SQLBacked[T] {}
+  // typeable stuff doesn't work, got to figure out how to stringify keys tomorrow */
+  implicit def sqlbacked[T, R <: HList, O <: HList, C](implicit labelgen: LabelledGeneric.Aux[T, R], ks: Keys.Aux[R, O], tta: ToList[O, C], typ: Typeable[C], ts: ToString[T]) = 
+    new SQLBacked[T] {
+      type Repr = R
+      type Keys = O
+      
+      val className = ts.apply
+      val tableName = ts.toTableName
+      val keys = ks.apply
+    }
 }
 
 trait ToString[T] {
@@ -24,12 +46,32 @@ object ToString {
   }
 }
 
+trait SubSeq[L <: HList] {
+  type Sub <: HList
+}
+
+trait LowPrioritySubSeq {
+  implicit def hconsSubSeq[H, T <: HList, SH, ST <: HList](implicit subseq: SubSeq.Aux[T, SH :: ST]): SubSeq.Aux[H :: T, SH :: ST] = new SubSeq[H :: T] { type Sub = SH :: ST }
+}
+
+object SubSeq extends LowPrioritySubSeq {
+  def apply[L <: HList, S <: HList](implicit subseq: SubSeq.Aux[L, S]) = subseq
+  
+  type Aux[L <: HList, S <: HList] = SubSeq[L] { type Sub = S }
+  
+  implicit def hnilSubSeq: SubSeq.Aux[HNil, HNil] = new SubSeq[HNil] { type Sub = HNil }
+  implicit def hconsSubSeqIso[H, SH, T <: HList, ST <: HList](implicit iso: H =:= SH, subseq: SubSeq.Aux[T, ST]): SubSeq.Aux[H :: T, SH :: ST] = new SubSeq[H :: T] { type Sub = SH :: ST }
+}
+
 object SQL {
  // import SQLBacked._
-  //def using[T <: DB] 
-  def create[T: SQLBacked](implicit ts: ToString[T]): String = {
-    val tableName = ts.toTableName
-    s"CREATE TABLE $tableName ();"
+  //def using[T <: DB]
+  def table[T](implicit sql: SQLBacked[T]): SQLBacked.Aux[T, sql.Repr, sql.Keys] = sql
+  
+  def create[T: SQLBacked] = {
+    val table = SQL.table[T]
+    s"CREATE TABLE ${table.tableName} (${table.keys.toList.cast[List[Symbol]].get});" //yolo!
+    table.keys
   }
   
   def insert[T: SQLBacked](row: T) = ???
